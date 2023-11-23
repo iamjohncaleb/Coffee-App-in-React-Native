dce67975cb101e9778b6b7bc5a7c805c4d2d34da
Caleb John <johncaleb022@gmail.com>
2023-11-23 03:41:56 -0800

    Update App.js

--- App.js
+++ App.js
@@ -3,10 +3,8 @@ import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import AppNavigation from './navigation/appNavigation';
 
 
-
 export default function App() {
   return (
-    
     <AppNavigation />
   );
 }
